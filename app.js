bc3bd2d608e18ea48d73422fd6b8905cd8444d1b
Papanister <225164648+janykkaloy-png@users.noreply.github.com>
2026-01-17 13:41:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ const FIXED_EMPLOYEE_STRUCTURE = [
     { id: 'curator', position: 'Куратор ВП', type: 'curator', username: 'Chaffy_Washington' },
     { id: 'senior_officer_1', position: 'Старший офицер ВП', type: 'senior_officer', username: 'Вакантно' },
     { id: 'senior_officer_2', position: 'Старший офицер ВП', type: 'senior_officer', username: 'Вакантно' },
-    { id: 'officer_1', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
-    { id: 'officer_2', position: 'Офицер ВП', type: 'officer', username: 'Angel_Extazzy' },
-    { id: 'officer_3', position: 'Офицер ВП', type: 'officer', username: 'Crux_Red' },
-    { id: 'officer_4', position: 'Офицер ВП', type: 'officer', username: 'Goose_Playboy' },
+    { id: 'officer_1', position: 'Офицер ВП', type: 'officer', username: 'Cheer_Queensight' },
+    { id: 'officer_2', position: 'Офицер ВП', type: 'officer', username: 'Crux_Red' },
+    { id: 'officer_3', position: 'Офицер ВП', type: 'officer', username: 'Goose_Playboy' },
+    { id: 'officer_4', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'officer_5', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'officer_6', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'officer_7', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
-    { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'Cheer_Queensight' },
+    { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'Matwey_Valhalla' },
     { id: 'cadet_2', position: 'Курсант ВП', type: 'cadet', username: 'Denis_Thompson' },
-    { id: 'cadet_3', position: 'Курсант ВП', type: 'cadet', username: 'Matwey_Valhalla' }
+    { id: 'cadet_3', position: 'Курсант ВП', type: 'cadet', username: 'Lolly_Dolly' }
 ];
 
 
@@ -3039,6 +3039,7 @@ function renderAdmin() {
 
 // --- ИНИЦИАЛИЗАЦИЯ ---
 document.addEventListener('DOMContentLoaded', initUI);
+
 
 
 
