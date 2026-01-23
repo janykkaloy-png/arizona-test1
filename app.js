c720ef2d4798dab324ec284ef0b64d170f8fb9dc
Papanister <225164648+janykkaloy-png@users.noreply.github.com>
2026-01-23 23:48:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ let playersDatabase = JSON.parse(localStorage.getItem('playersDatabase') || '[]'
 
 const FIXED_EMPLOYEE_STRUCTURE = [
     { id: 'curator', position: 'Куратор ВП', type: 'curator', username: 'Chaffy_Washington' },
-    { id: 'senior_officer_1', position: 'Старший офицер ВП', type: 'senior_officer', username: 'Вакантно' },
+    { id: 'senior_officer_1', position: 'Старший офицер ВП', type: 'senior_officer', username: 'Crux_Red' },
     { id: 'senior_officer_2', position: 'Старший офицер ВП', type: 'senior_officer', username: 'Вакантно' },
     { id: 'officer_1', position: 'Офицер ВП', type: 'officer', username: 'Cheer_Queensight' },
     { id: 'officer_2', position: 'Офицер ВП', type: 'officer', username: 'Denis_Thompson' },
-    { id: 'officer_3', position: 'Офицер ВП', type: 'officer', username: 'Crux_Red' },
+    { id: 'officer_3', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'officer_4', position: 'Офицер ВП', type: 'officer', username: 'Goose_Playboy' },
     { id: 'officer_5', position: 'Офицер ВП', type: 'officer', username: 'Akashi_Miyazuki' },
     { id: 'officer_6', position: 'Офицер ВП', type: 'officer', username: 'Matwey_Valhalla' },
@@ -3039,6 +3039,7 @@ function renderAdmin() {
 
 // --- ИНИЦИАЛИЗАЦИЯ ---
 document.addEventListener('DOMContentLoaded', initUI);
+
 
 
 
