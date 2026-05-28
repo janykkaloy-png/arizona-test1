1974d36bb4a8222742c6b8b66f9343472a56f47c
Papanister <225164648+janykkaloy-png@users.noreply.github.com>
2026-05-29 00:21:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const FIXED_EMPLOYEE_STRUCTURE = [
     { id: 'officer_5', position: 'Офицер ВП', type: 'officer', username: 'Ralph_Laurence' },
     { id: 'officer_3', position: 'Офицер ВП', type: 'officer', username: 'Dear_Scaletto' },
     { id: 'officer_1', position: 'Офицер ВП', type: 'officer', username: 'Watson_Peterson' },
-    { id: 'officer_6', position: 'Офицер ВП', type: 'officer', username: 'Goro_Lunterra' },
     { id: 'officer_2', position: 'Офицер ВП', type: 'officer', username: 'Eul_Requiem' },
     { id: 'officer_7', position: 'Офицер ВП', type: 'officer', username: 'Danya_Armani' },
+    { id: 'officer_6', position: 'Офицер ВП', type: 'officer', username: 'Вакантно' },
     { id: 'cadet_2', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
     { id: 'cadet_1', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' },
     { id: 'cadet_3', position: 'Курсант ВП', type: 'cadet', username: 'Вакантно' }
